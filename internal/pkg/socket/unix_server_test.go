fe72f08496e949c35a70756039c09ab56da219d7
micronull <2688692+micronull@users.noreply.github.com>
2022-07-08 17:00:50 +0500

    make unix sock

---
+++ internal/pkg/socket/unix_server_test.go
@@ -0,0 +1,26 @@
+package socket_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/micronull/i3rotonda/internal/pkg/socket"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+	ch := make(chan bool)
+
+	addr := socket.Run(func(conn *net.Conn) {
+		ch <- true
+	})
+
+	go func() {
+		_, err := net.Dial("unix", addr.String())
+		assert.NoError(t, err)
+	}()
+
+	assert.Eventually(t, func() bool { return <-ch }, time.Second*5, time.Millisecond, "no connected")
+}
